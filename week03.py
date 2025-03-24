93f55687f6e3ec9ee8bf5e952fd7783da213c19f
douni05 <douni05@gmail.com>
2025-03-24 12:08:37 +0900

    v1.0 array

--- week03.py
+++ week03.py
@@ -1,18 +1,10 @@
-def duplicate(cities):
-    result = list()
-    s = set()
+def intersection(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append()
+    return l3
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2: # 중복값이 들어옴
-            result.append(city)
-    return result
-
-cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-# city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-cities.append('Anyang')
-cities.append('Seoul')
-print(cities)
-print(duplicate(cities))
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [45, 5, 22, 31, 7, 38, 27, 19, 13, 41]
+print(intersection(l1, l2))
