c0a71a8380edb6174658234c399c8b4beeea03a5
douni05 <douni05@gmail.com>
2025-04-07 11:36:55 +0900

    v2.1 stack

--- week05.py
+++ week05.py
@@ -1,33 +1,32 @@
-class Steck:
-    def __init__(self):
-        self.items = list()
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
 
-    def push(self, item):
-        self.items.append(item)
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.link = self.top
+            self.top = node
 
     def pop(self):
-        return self.items.pop()
+        if self.top is None:
+            return "Stack is empty: Noting to data"
+            #raise IndexError("스택이 비어있습니다.")
+        popped_node = self.top
+        self.top = self.top.link
+        return popped_node.data
 
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        return self.items[-1]
-
-
-s1 = Steck()
-s2 = Steck()
-print(s1.is_empty())
-s1.push("자료구조")
-print(s1.is_empty())
-print(s2.is_empty())
-s1.push("데이터베이스")
-print(s1.size())  #2
-print(s1.peek())
-print(s1.size())  #2
+s1 = Stack()
 print(s1.pop())
-print(s1.size())  #1
-print(s1.peek())
+s1.push("data structure")
+s1.push("database")
+print(s1.pop())
+print(s1.pop())
