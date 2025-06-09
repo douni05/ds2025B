8dea9a2cb7dc8e02c51d5101d90c149e2fa4045d
douni05 <douni05@gmail.com>
2025-06-09 10:06:51 +0900

    v4.9 graph

--- week12.py
+++ week12.py
@@ -1,39 +1,9 @@
-from collections import deque
+v, e = map(int, input().split()) #정점, 간선
+edges = list()
 
-graph = [
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0]
-]
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
 
-def dfs(g, i, visited):
-    visited[i] = 1
-    print(chr(ord('A')+i), end=' ')
-    for j in range(len(g)):
-        if g[i][j] == 1 and not visited[j]:
-            dfs(g, j, visited)
+print(edges)
 
-
-def bfs(g, i, visited):
-    queue = deque([i])  # popleft, append
-    visited[i] = True
-    while queue:
-        # print(visited)
-        i = queue.popleft()  # dequeue
-        print(chr(ord('A') + i), end=' ')
-        for j in range(len(g)):
-            if g[i][j] == 1 and not visited[j]:
-                queue.append(j)
-                visited[j] = True
-
-
-visited_dfs = [0 for _ in range(len(graph))]
-visited_bfs = [False for _ in range(len(graph))]
-dfs(graph, 6, visited_dfs)
-print()
-bfs(graph, 1, visited_bfs)
